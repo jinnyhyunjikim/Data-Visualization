d615f1c0baffeaf206403bea0d6a66b3dc454c7a
Jinny HyunJi Kim <hyunjiki@andrew.cmu.edu>
2016-03-15 20:16:43 -0400

    setup_mailer.rb added to config and mailer model created

---
+++ Desktop/67272/Friends/config/initializers/setup_mailer.rb
@@ -0,0 +1,8 @@
+  ActionMailer::Base.smtp_settings = {  
+    :address              => "smtp.gmail.com",  
+    :port                 => 587,  
+    :user_name            => "jinny6235@gmail.com",  
+    :password             => "319535guswl319535Guswl>",  
+    :authentication       => "plain",  
+    :enable_starttls_auto => true  
+  }
